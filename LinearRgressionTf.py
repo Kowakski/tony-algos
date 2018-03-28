0737984e713d7c73b4bc1ba3c6a3c157bb57762b
linshen <linshen@pacewear.cn>
2018-03-28 16:07:47 +0800

    write linear regression codes with tensorflow

---
+++ LinearRgressionTf.py
@@ -0,0 +1,44 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+#Create Data use numpy
+xTrain = np.random.random(500)
+noise = np.random.normal(0, 0.05, xTrain.shape).astype(np.float32)
+yTrain = 6*xTrain+ 1 + noise  #Get 6 and 1
+fig = plt.figure( )
+ax1 = fig.add_subplot(111)
+ax1.scatter(xTrain, yTrain)
+# plt.show( )
+
+xInput = tf.placeholder( dtype = tf.float32 )
+# xInput = tf.Print(xInput,[xInput], 'xInput:', first_n = 5)
+
+yInput = tf.placeholder( dtype = tf.float32 )
+
+# weight = tf.Variable( 0.3, name='value', dtype = tf.float32 )
+weight = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+
+# weight = tf.Print(weight,[weight], 'weights:', first_n = 500)
+
+# bias = tf.Variable( 1, name='bias', dtype = tf.float32 )
+bias = tf.Variable(tf.zeros([1]))
+bias = tf.Print(bias,[bias], 'bias:', first_n = 5)
+
+OutPut = tf.multiply( weight, xInput ) + bias
+# OutPut = tf.Print(OutPut,[OutPut], 'OutPut:', first_n = 5)
+
+loss = tf.reduce_mean( tf.square( OutPut - yInput ) )
+loss = tf.Print(loss, [loss], "loss is", summarize = 1000)
+
+optimizer = tf.train.GradientDescentOptimizer( 0.3 )
+train = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer( )
+sess = tf.Session( )
+sess.run(init)
+
+for i in range(200):
+    sess.run( train, feed_dict={ xInput:xTrain, yInput:yTrain } )
+
+print("weights is:{0} bias is:{1}".format( sess.run(weight), sess.run(bias) ))
