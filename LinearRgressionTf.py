f0c78cbadc79872784df7bda74f9bb38bfaca492
linshen <linshen@pacewear.cn>
2018-03-29 15:57:24 +0800

    linear regression is 2 dimension

--- LinearRgressionTf.py
+++ LinearRgressionTf.py
@@ -52,46 +52,36 @@ ax1.plot(xTrain, y, 'g')
 plt.show( )
 '''
 
-
 #Create Data use numpy
-X = np.random.random(50)
-Y = np.random.random(50)
-feature = []
-for value in zip(X,Y):
-    feature.append(list(value))
-# print(feature)
-
+X = np.random.random(500)
+Y = np.random.random(500)
 noise = np.random.normal(0, 0.1, X.shape).astype(np.float32)
 Z = 6*X+ +5*Y + 1 + noise  #Get 6 and 1
 
-label = Z
+feature = []
+for value in zip(X,Y):
+    feature.append(list(value))
 
-print("feature is: {0} ".format( type(feature) ) )
-print("label is: {0}".format( label ) )
+labels = np.array(Z).reshape(len(Z), 1)
 
 fig = plt.figure( )
 ax1 = Axes3D(fig)
-ax1.scatter( X, Y, Z )
-X,Y = np.meshgrid(X,Y)
-zWithoutNoise = 6*X+ +5*Y + 1
-ax1.plot_surface( X, Y, zWithoutNoise,  alpha = 0.8 )
- 
+ax1.scatter( X, Y, Z,'r' )
+
+# zWithoutNoise = 6*X+ +5*Y + 1
+# ax1.plot_surface( X, Y, zWithoutNoise,  alpha = 0.8 )
+
 xInput = tf.placeholder( dtype = tf.float32, shape = ( None, 2 ) )
-# xInput = tf.Print(xInput,[xInput], 'xInput:', first_n = 5)
 
-yInput = tf.placeholder( dtype = tf.float32, shape =( None, 1 ) )
+yInput = tf.placeholder( dtype = tf.float32, shape = (None, 1) )
 
-# weight = tf.Variable( 0.3, name='value', dtype = tf.float32 )
 weight = tf.Variable(tf.random_uniform( [2,1], -1.0, 1.0 ) )
+weight = tf.Print(weight,[weight], 'weights:', first_n = 5 )
 
-# weight = tf.Print(weight,[weight], 'weights:', first_n = 500)
-
-# bias = tf.Variable( 1, name='bias', dtype = tf.float32 )
 bias = tf.Variable(tf.zeros([1]))
-bias = tf.Print(bias,[bias], 'bias:', first_n = 5)
+bias = tf.Print(bias,[bias], 'bias:', first_n = 5 )
 
-OutPut = tf.multiply( xInput, weight ) + bias
-# OutPut = tf.Print(OutPut,[OutPut], 'OutPut:', first_n = 5)
+OutPut = tf.matmul( xInput, weight ) + bias
 
 loss = tf.reduce_mean( tf.square( OutPut - yInput ) )
 loss = tf.Print(loss, [loss], "loss is", summarize = 1000)
@@ -104,10 +94,18 @@ sess = tf.Session( )
 sess.run(init)
 
 for i in range(200):
-    sess.run( train, feed_dict={ xInput:feature, yInput:Z } )
+    sess.run( train, feed_dict={ xInput:feature, yInput:labels } )
+FinalWeights = sess.run(weight)
+FinalBias = sess.run(bias)
+a = FinalWeights[0]
+b = FinalWeights[1]
+print("weights is:{0} {1} bias is:{2}".format( a, b , FinalBias ) )
 
-print("weights is:{0} bias is:{1}".format( sess.run(weight), sess.run(bias) ))
-# w = sess.run(weight)
-# b = sess.run(bias)
-# y = x*w + b
-# ax1.plot(x, y, 'g')
+X,Y = np.meshgrid(X,Y)
+c = a*X+b*Y+FinalBias
+# ax1.plot_surface(X,Y,c, rstride=1, cstride=1, cmap=plt.cm.coolwarm,alpha=0.5)
+ax1.plot_surface(X,Y,c, rstride=200, cstride=200, cmap=plt.cm.coolwarm, alpha=0.8)
+ax1.set_xlabel('f1', color='r')
+ax1.set_ylabel('f2', color='g')
+ax1.set_zlabel('label', color='b')#给三个坐标轴注明
+plt.show()
