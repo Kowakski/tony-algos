de40d26eefa384f01a520f1d5865e2942fdf8c24
linshen <linshen@pacewear.cn>
2018-03-30 18:27:05 +0800

    linear incoming file

--- linearregression/IncomingPre.py
+++ linearregression/IncomingPre.py
@@ -1,33 +1,96 @@
 import os
 import tensorflow as tf
+import tensorflow.contrib.eager as tfe
 
+tfe.enable_eager_execution()
 
 TrainDataPath = "../woodata/adult.data"
 TestDataPath = "../woodata/adult.test"
 
 mark = ["age", "workclass", "fnlwgt", "education", "education_num", "marital_status", "occupation", "relationship", "race", "gender", "capital_gain", "capital_loss", "hours_per_week", "native_country", "income"]
+FIELD_DEFAULTS = [ [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]]
+
+def __parse_line( line ):
+    feature = tf.decode_csv( line, record_defaults =  FIELD_DEFAULTS )
+    # feature = tf.Print(feature, [feature])
+    feature = dict(zip(mark, feature))
+    label = feature.pop( "income" )
+    print("FEATURE IS**:{0}\r\n".format(feature))
+    return feature, label
+
 #define input function
 def data_input_fun( path, batchsize ):
     if not os.path.exists(path):
         return
     data = tf.data.TextLineDataset(path)
+    data.map(__parse_line)
     return data
-    # print(data.output_classes)
-    # print(data.output_shapes)
-    # print(data.output_types)
 
 #define feature columns
+#convert string to interger
+age = tf.feature_column.numeric_column(
+    key = 'age' )
+
+education_num = tf.feature_column.numeric_column(
+    key = 'education_num' )
+
+capital_gain = tf.feature_column.numeric_column(
+    key = 'capital_gain' )
+
+capital_loss = tf.feature_column.numeric_column(
+    key = 'capital_gain' )
+
+hours_per_week = tf.feature_column.numeric_column(
+    key = 'hours_per_week' )
+
+workclass = tf.feature_column.categorical_column_with_vocabulary_list(
+    key = 'workclass',
+    vocabulary_list = [ 'Self-emp-not-inc', 'Private', 'State-gov', 'Federal-gov',
+        'Local-gov', '?', 'Self-emp-inc', 'Without-pay', 'Never-worked' ] )
+
+fnlwgt = tf.feature_column.categorical_column_with_vocabulary_list(
+    key = '',
+    vocabulary_list = [] )
+education = tf.feature_column.categorical_column_with_vocabulary_list(
+    key = 'education',
+    vocabulary_list = ['Bachelors', 'HS-grad', '11th', 'Masters', '9th', 'Some-college',
+        'Assoc-acdm', 'Assoc-voc', '7th-8th', 'Doctorate', 'Prof-school',
+        '5th-6th', '10th', '1st-4th', 'Preschool', '12th'] )
+
+marital_status = tf.feature_column.categorical_column_with_vocabulary_list(
+    key = 'marital_status',
+    vocabulary_list = ['Married-civ-spouse', 'Divorced', 'Married-spouse-absent',
+        'Never-married', 'Separated', 'Married-AF-spouse', 'Widowed'] )
+
+occupation = tf.feature_column.categorical_column_with_vocabulary_list(
+    key = '',
+    vocabulary_list = ['Adm-clerical', 'Exec-managerial', 'Handlers-cleaners', 'Prof-specialty', 'Other-service', 'Sales', 'Craft-repair', 'Transport-moving', 'Farming-fishing', 'Machine-op-inspct', 'Tech-support', '?', 'Protective-serv', 'Armed-Forces', 'Priv-house-serv'] )
+
+relationship = tf.feature_column.categorical_column_with_vocabulary_list(
+    key = '',
+    vocabulary_list = ['Husband', 'Not-in-family', 'Wife', 'Own-child', 'Unmarried',
+        'Other-relative'] )
+
+# race = tf.feature_column.categorical_column_with_vocabulary_list(
+#     key = '',
+#     vocabulary_list = [] )
+
+gender = tf.feature_column.categorical_column_with_vocabulary_list(
+    key = 'gender',
+    vocabulary_list = ['Male','Female'] )
+
+native_country = tf.feature_column.categorical_column_with_vocabulary_list(
+    key = 'native_country',
+    vocabulary_list = [] )
+
+income = tf.feature_column.categorical_column_with_vocabulary_list(
+    key = '',
+    vocabulary_list = [] )
 
 #define estimator
 
 #evaluate
-
-with tf.Session() as sess:
-    data = data_input_fun(TrainDataPath, 500)
-    iterator = data.make_one_shot_iterator()
-    one_element = iterator.get_next()
-    try:
-        while True:
-            print(sess.run(one_element))
-    except tf.errors.OutOfRangeError:
-        print("end!")
+data = data_input_fun(TrainDataPath, 500)
+iterator = tfe.Iterator(data)
+# for  one_item in iterator:
+#     print( one_item )
--- myestimator.py
+++ myestimator.py
@@ -19,12 +19,16 @@ TrainDataPath = './woodata/iris_training.csv'
 EvaluDataPath = './woodata/iris_test.csv'
 
 def __parse_line(line):
+    # line = tf.Print(line, [line], message = "line is:", first_n =5)
     fields = tf.decode_csv( line, FIELD_DEFAULTS )
     features = dict(zip(COLUMNS, fields))
+    print( "feature is:{0}\r\n".format( features ) )
     labels = features.pop('label')
+    # labels = tf.Print(labels, [labels], message = "labels", first_n = 5)
     return features, labels
 
 def data_input_function(PATH, BatchSize):
+    print("**I'm input function**\r\n")
     Data = tf.data.TextLineDataset( PATH, compression_type = None, buffer_size = None ).skip(1)
     Data = Data.map(__parse_line)
     Data = Data.shuffle(1000).repeat().batch(BatchSize)
@@ -45,7 +49,7 @@ feature_columns = [tf.feature_column.numeric_column(key=x, shape=(1,),default_va
 def my_model_fun( features, labels, mode, params ):
     #input layer, convert feature dictionary to feature
     net = tf.feature_column.input_layer(features = features, feature_columns = params['feature_columns'] )      #!!! important API
-    net = tf.Print(net, [net], message ="Input net", first_n = 1, summarize = 10000)
+    # net = tf.Print(net, [net], message ="Input net", first_n = 1, summarize = 5)
     # print("input feature is:{0}\r\n".format(features) )
 
     #hidden layers
@@ -65,7 +69,7 @@ def my_model_fun( features, labels, mode, params ):
         }
         return tf.estimator.EstimatorSpec( mode, predictions = predictions )
 
-    labels = tf.Print(labels, [labels], message = "Cross entropy label is", first_n=1, summarize=10000)
+    # labels = tf.Print(labels, [labels], message = "Cross entropy label is", first_n=1, summarize=10000)
     loss = tf.losses.sparse_softmax_cross_entropy( labels = labels, logits = logits ) #!!! Important API
 
     #evaluate, return requires loss
@@ -91,7 +95,7 @@ classifier = tf.estimator.Estimator(
     },
 
 )
-classifier.train(input_fn = lambda: data_input_function(TrainDataPath, 200), hooks=None, steps=200)
+classifier.train(input_fn = lambda: data_input_function(TrainDataPath, 2), hooks=None, steps=1)
 
-evaluation = classifier.evaluate(input_fn = lambda: data_input_function(EvaluDataPath, 200), steps=1, name='evaluation')
-print("evaluation is {0}\r\n".format( evaluation ))
+# evaluation = classifier.evaluate(input_fn = lambda: data_input_function(EvaluDataPath, 200), steps=1, name='evaluation')
+# print("evaluation is {0}\r\n".format( evaluation ))
