3cdd540eb78caadd921252e9a0fbf295a415a60a
linshen <linshen@pacewear.cn>
2018-04-19 19:58:45 +0800

    mod cnn file

--- image/cnn.py
+++ image/cnn.py
@@ -1,29 +1,48 @@
 '''
 @Create a customer estimator for cnn network
 '''
+import numpy as np
 import tensorflow as tf
 import tensorflow.contrib.eager as tfe
 
 # tfe.enable_eager_execution()
 # from tensorflow.python import debug as tf_debug
 tf.logging.set_verbosity(tf.logging.INFO)
-
+np.set_printoptions(threshold=np.nan)
+# print(ndarray)
 #define input function
 
 #define feature columns
 
 #define estimator function
+def mod_fn( self, features, labels, mode, params ):
+    #convolution layer
+    net = tf.layers.Conv2D.conv2d(inputs = features, filters = , kernel_size = , stride = (1,1), padding = 'valid'):
+
+    #pooling layer
+    net = tf.layers.AveragePooling2D.average_pooling2d( inputs = , pool_size = , strides = , padding='valid', data_format='channels_last', name=None )
+
+    #convolution layer
+
+    #pooling layer
+
+    #full connected layer
+
+    #softmax layer
 
 #define main function
 def main(unused_args):
     data = tf.contrib.learn.datasets.mnist.load_mnist()
-    # iterator = data.make_one_shot_iterator()
-    # item = iterator.get_next()
+    TrainImages = data.train.images
+    TrainLabels = data.train.labels
+    EvaluImages = data.validation.images
+    EvaluLabels = data.validation.labels
 
-    tf.logging.log(tf.logging.INFO, data)
-    tf.logging.log(tf.logging.INFO, data.train)
-    tf.logging.log(tf.logging.INFO, data.validation)
+    print((TrainImages.shape))
+    print((TrainLabels.shape))
 
+    tf.logging.log( tf.logging.INFO, TrainImages[:1] )
+    tf.logging.log( tf.logging.INFO, TrainLabels[:1] )
 
 if __name__ == "__main__":
     tf.app.run()
