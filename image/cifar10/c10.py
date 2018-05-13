837cde1020237e537a776d8152d57a4000690a84
linshen <linshen@pacewear.cn>
2018-05-13 17:24:06 +0800

    add cifar10

---
+++ image/cifar10/c10.py
@@ -0,0 +1,6 @@
+#This is classification for 10 animals:
+import sys
+import cifar10_input
+
+sys.path.append("../")
+print("import cifar_input")
---
+++ image/cifar10/cifar10/Download.py
@@ -0,0 +1,2 @@
+import cifar10
+cifar10.maybe_download_and_extract()
