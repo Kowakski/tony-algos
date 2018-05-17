e60be4e49f540626b03f1e2136506399287e7680
linshen <linshen@pacewear.cn>
2018-05-17 20:40:53 +0800

    /*  *@author:  *@type:  *@commit:添加CIR10数据集的说明  */

--- image/cifar10/c10.py
+++ image/cifar10/c10.py
@@ -1,4 +1,24 @@
 #This is classification for 10 animals:
+'''
+#About Cifar10 Data
+The CIFAR-10 dataset consists of 60000 32x32 colour images in 10 classes, with 6000 images per class.
+There are 50000 training images and 10000 test images.
+The dataset is divided into five training batches and one test batch, each with 10000 images.
+The test batch contains exactly 1000 randomly-selected images from each class.
+The training batches contain the remaining images in random order, but some training batches may contain more images from one class than another.
+Between them, the training batches contain exactly 5000 images from each class.
+Here are the classes in the dataset, as well as 10 random images from each:
+airplane
+automobile
+bird
+cat
+deer
+dog
+frog
+horse
+ship
+truck
+'''
 import sys
 import numpy as np
 # from cifar10 import cifar10_input
