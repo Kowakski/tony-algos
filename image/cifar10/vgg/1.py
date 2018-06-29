d9a1043c0e960c3c890bea2eb6940f9f659fcc98
linshen <linshen@pacewear.cn>
2018-06-29 21:40:30 +0800

    my input is work

--- image/cifar10/vgg/1.py
+++ image/cifar10/vgg/1.py
@@ -1,1 +1,11 @@
+import numpy as np
 from cifar10_input import C10Input
+
+c10input = C10Input('/tmp/cifar-10-batches-py')
+
+data, label = c10input.get_batch_data(10)
+
+print(type(data))
+print(np.shape(data))
+# print(data)
+print(np.shape(label))
--- image/cifar10/vgg/cifar10_input.py
+++ image/cifar10/vgg/cifar10_input.py
@@ -3,11 +3,12 @@ import numpy  as np
 import pickle as p
 
 class C10Input(object):
-    def get_data(DataPath):
-        fb = open(DataPath + 'data_batch_%d' % i)
+    def get_data(self, DataPath):
+        f = open(DataPath + '/data_batch_%d' % self.FileIndex, 'rb')
         datadict = p.load(f, encoding='bytes')
         X = datadict[b'data']
         Y = datadict[b'labels']
+        self.FileIndex = self.FileIndex + 1
         return X,Y
 
     """docstring for C10Input"""
@@ -16,23 +17,25 @@ class C10Input(object):
         assert DataPath is not None
         self.index     = 0
         self.Path      = DataPath
-        self.FileIndex = 0
+        self.FileIndex = 1
 
-    def get_data(self, batchsize):
+    def get_batch_data(self, batchsize):
         if(self.index == 0):
-            data, label = self.get_data()
+            data, label = self.get_data(self.Path)
 
         if((self.index + batchsize) < 10000):
             batch_data, batch_label = data[ self.index: self.index+batchsize ], label[ self.index: self.index+batchsize ]
             self.index = self.index+1
 
-        if(index<10000) and (self.index + batchsize) > 10000:
+        if(self.index<10000) and (self.index + batchsize) > 10000:
             batch_data, batch_label = data[self.index : 9999], label[self.index:9999]
             data, label = self.get_data( )
             self.index = self.index + batchsize - 9999  #batchsize - (9999-self.index)
             batch_data = np.append( batch_data, data[0:self.index] )
             batch_label = np.append( batch_label, label[0:self.index] )
 
-            batch_data = np.reshape( batch_data, [batchsize, 32, 32, 3] )
-            batch_label = np.reshape( batch_label, [batchsize, 1] )
+        # batch_data = np.reshape( batch_data, [batchsize, 32, 32, 3] )
+        batch_data = batch_data.reshape( [batchsize, 32, 32, 3] )
+        batch_label = np.reshape( batch_label, [batchsize, 1] )
+
         return batch_data, batch_label
