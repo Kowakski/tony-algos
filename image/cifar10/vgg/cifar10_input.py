afe4eeb45a2c6386542297f92cf68817165fbda4
linshen <linshen@pacewear.cn>
2018-07-01 14:10:14 +0800

    /*@commit:获取cifar10数据  *@author:sln  *@type:  *@commit details: 获取cifar10数据正常  */

--- image/cifar10/vgg/cifar10_input.py
+++ image/cifar10/vgg/cifar10_input.py
@@ -9,6 +9,8 @@ class C10Input(object):
         X = datadict[b'data']
         Y = datadict[b'labels']
         self.FileIndex = self.FileIndex + 1
+        if self.FileIndex > 5:
+            self.FileIndex = 1
         return X,Y
 
     """docstring for C10Input"""
@@ -19,23 +21,41 @@ class C10Input(object):
         self.Path      = DataPath
         self.FileIndex = 1
 
+    def merge_channel( self, Data ):
+        assert len(np.shape(Data)) == 4
+        r = Data[:,0][:,:,:,np.newaxis]    #batchsize*32*32
+        g = Data[:,1][:,:,:,np.newaxis]
+        b = Data[:,2][:,:,:,np.newaxis]
+
+        result = np.append( r,g, axis = 3 )
+        result = np.append( result, b, axis = 3 )
+
+        return result
+
     def get_batch_data(self, batchsize):
         if(self.index == 0):
-            data, label = self.get_data(self.Path)
+            self.data, self.label = self.get_data(self.Path)
 
-        if((self.index + batchsize) < 10000):
-            batch_data, batch_label = data[ self.index: self.index+batchsize ], label[ self.index: self.index+batchsize ]
-            self.index = self.index+1
+        # print(self.index)
+        if((self.index + batchsize) <= 10000):
+            # print("here 1")
+            batch_data, batch_label = self.data[ self.index: self.index+batchsize ], self.label[ self.index: self.index+batchsize ]
+            self.index = self.index+batchsize
+            if(self.index == 10000):
+                self.index = 0
 
         if(self.index<10000) and (self.index + batchsize) > 10000:
-            batch_data, batch_label = data[self.index : 9999], label[self.index:9999]
-            data, label = self.get_data( )
+            # print("here 2")
+            batch_data, batch_label = self.data[self.index : 9999], self.label[self.index:9999]
+            self.data, self.label = self.get_data( self.Path )
             self.index = self.index + batchsize - 9999  #batchsize - (9999-self.index)
-            batch_data = np.append( batch_data, data[0:self.index] )
-            batch_label = np.append( batch_label, label[0:self.index] )
+            batch_data = np.append( batch_data, self.data[0:self.index] )
+            batch_label = np.append( batch_label, self.label[0:self.index] )
 
         # batch_data = np.reshape( batch_data, [batchsize, 32, 32, 3] )
-        batch_data = batch_data.reshape( [batchsize, 32, 32, 3] )
+        batch_data = batch_data.reshape( [batchsize, 3, 32, 32] )
+
+        batch_data = self.merge_channel(batch_data)
         batch_label = np.reshape( batch_label, [batchsize, 1] )
 
         return batch_data, batch_label
